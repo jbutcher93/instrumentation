ff9964ea7ac76135e088dda97f6111252f2c5a91
John Butcher <95865907+jbutcher93@users.noreply.github.com>
2022-12-16 16:36:51 -0500

    Update main.go

--- main.go
+++ main.go
@@ -6,14 +6,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type Message struct {
-	Sentence string `json:"sentence"`
-}
-
-func message(c *gin.Context) {
-	c.JSON(200, &Message{"This is a test message"})
-}
-
 func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
